f4ff5a118b64e22215292cce81af87320a983e3a
Kirito377 <140883441+Kirito377@users.noreply.github.com>
2023-08-24 18:40:11 +0530

    Update Q3_Factorial_number_using_do_while_loop.c

---
+++ Codes/Q3_Factorial_number_using_do_while_loop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, sum, i = 1;
+    printf("Enter the number 1: ");
+    scanf("%d", &a);
+    if (a <= 0)
+    {
+        printf("0 and Negative numbers are not allowed");
+    }
+    else
+    {
+        sum = 1;
+        do
+        {
+            sum *= i;
+            i++;
+        } while (i <= a);
+        printf("%d", sum);
+    }
+
+    return 0;
+}
