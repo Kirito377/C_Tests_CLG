68f552c6ef5c8e77373703264a384c5a42bc3d2e
Kirito377 <140883441+Kirito377@users.noreply.github.com>
2023-08-24 16:46:46 +0530

    Create Adding_two_number_using_float.c

---
+++ Adding_two_number_using_float.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    float a,b,sum;
+    printf("Enter the number 1: ");
+    scanf("%f", &a);
+    printf("Enter the number 2: ");
+    scanf("%f", &b);
+    sum = a+b;
+    printf("Answer is : %f", sum);
+    return 0;
+}
